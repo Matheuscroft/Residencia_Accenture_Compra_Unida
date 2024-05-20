04001becd84e2e5ae339ebe462eb80f9c29f977a
RenanOliveira04 <rlsorenan@gmail.com>
2024-05-20 12:40:50 -0300

    melhorias visuais

--- src/components/CadastroFornecedor.jsx
+++ src/components/CadastroFornecedor.jsx
@@ -1,76 +1,118 @@
 import React, { useState } from "react";
 import NavegacaoHeader from "./NavegacaoHeader";
-import { Input, Wrapper, Title, Button, BotaoPrincipal } from "./Estilos";
+import { Form, Button, Container, Row, Col, Card, Alert } from "react-bootstrap";
 
 const CadastroFornecedor = (props) => {
-
     const [dadosFornecedor, setDadosFornecedor] = useState({});
+    const [errors, setErrors] = useState({});
 
     const handleChange = (event) => {
         const name = event.target.name;
-        const value = event.target.value;
-        //setDadosFornecedor(values => ({...values, [name]: value}))
-        setDadosFornecedor({ ...dadosFornecedor, [name]: value })
-        console.log(dadosFornecedor)
-    }
+        let value = event.target.value;
+
+        if (name === "telefone") {
+            value = formatTelefone(value);
+        }
+
+        setDadosFornecedor({ ...dadosFornecedor, [name]: value });
+        setErrors({ ...errors, [name]: '' });
+    };
+
+    const formatTelefone = (value) => {
+        // Remove tudo que não for dígito
+        value = value.replace(/\D/g, "");
+
+        // Formata o valor
+        if (value.length > 0) {
+            value = `(${value.substring(0, 2)}) ${value.substring(2, 3)}${value.substring(3, 7)}-${value.substring(7, 11)}`;
+        }
+
+        return value;
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert(`${dadosFornecedor.nome} cadastrado com sucesso!`)
-        props.handlePage("home-fornecedor")
-        
-      }
+        const newErrors = {};
+
+        // Verifica campos obrigatórios
+        ['cnpj_mei', 'endereco', 'telefone', 'email', 'senha', 'confirma_senha'].forEach(field => {
+            if (!dadosFornecedor[field]) {
+                newErrors[field] = 'Este campo é obrigatório';
+            }
+        });
+
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+        } else {
+            alert(`${dadosFornecedor.nome} cadastrado com sucesso!`);
+            props.handlePage("home-fornecedor");
+        }
+    };
 
     return (
-
-        <div>
+        <Container>
             <NavegacaoHeader />
+            <Row className="justify-content-md-center" style={{ marginTop: '100px' }}>
+                <Col xs={12} md={6}>
+                    <Card className="text-light" style={{ backgroundColor: '#1c3bc5', borderRadius: '15px', borderColor: '#d4edda' }}>
+                        <Card.Body>
+                            <h1 className="text-center text-light">Cadastro - Fornecedor</h1>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group controlId="razao_social" className="mb-3">
+                                    <Form.Label className="text-light">Razão Social</Form.Label>
+                                    <Form.Control type="text" name="razao_social" value={dadosFornecedor.razao_social || ""} onChange={handleChange} placeholder="Razão Social" />
+                                </Form.Group>
 
-            <Wrapper>
-                <form onSubmit={handleSubmit} >
-                    <Title>Cadastro - Fornecedor</Title>
+                                <Form.Group controlId="nome" className="mb-3">
+                                    <Form.Label className="text-light">Nome Completo do Responsável</Form.Label>
+                                    <Form.Control type="text" name="nome" value={dadosFornecedor.nome || ""} onChange={handleChange} placeholder="Nome Completo" />
+                                </Form.Group>
 
-                    <label>Razão Social:
-                        <Input type="text" name="razao_social" value={dadosFornecedor.razao_social || ""}  onChange={handleChange} placeholder="Razão Social" /><br />
-                    </label>
+                                <Form.Group controlId="cnpj_mei" className="mb-3">
+                                    <Form.Label className="text-light">CPNJ/MEI: OBG</Form.Label>
+                                    <Form.Control type="text" name="cnpj_mei" value={dadosFornecedor.cnpj_mei || ""} onChange={handleChange} placeholder="CPF/CPNJ" required />
+                                    {errors.cnpj_mei && <Alert variant="danger">{errors.cnpj_mei}</Alert>}
+                                </Form.Group>
 
-                    <p>{dadosFornecedor.razao_social}</p>
+                                <Form.Group controlId="endereco" className="mb-3">
+                                    <Form.Label className="text-light">Endereço: OBG</Form.Label>
+                                    <Form.Control type="text" name="endereco" value={dadosFornecedor.endereco || ""} onChange={handleChange} placeholder="Endereço" required />
+                                    {errors.endereco && <Alert variant="danger">{errors.endereco}</Alert>}
+                                </Form.Group>
 
-                    <label>Nome Completo do Responsável:
-                        <Input type="text" name="nome" value={dadosFornecedor.nome || ""}  onChange={handleChange} placeholder="Nome Completo" /><br />
-                    </label>
+                                <Form.Group controlId="telefone" className="mb-3">
+                                    <Form.Label className="text-light">Telefone: OBG</Form.Label>
+                                    <Form.Control type="text" name="telefone" value={dadosFornecedor.telefone || ""} onChange={handleChange} placeholder="Telefone" required />
+                                    {errors.telefone && <Alert variant="danger">{errors.telefone}</Alert>}
+                                </Form.Group>
 
-                    <label>CPNJ/MEI: OBRIGATORIO
-                        <Input type="text" name="cnpj_mei" value={dadosFornecedor.cnpj_mei || ""}  onChange={handleChange} placeholder="CPF/CPNJ" required /><br />
-                    </label>
+                                <Form.Group controlId="email" className="mb-3">
+                                    <Form.Label className="text-light">E-mail: OBG</Form.Label>
+                                    <Form.Control type="email" name="email" value={dadosFornecedor.email || ""} onChange={handleChange} placeholder="E-mail" required />
+                                    {errors.email && <Alert variant="danger">{errors.email}</Alert>}
+                                </Form.Group>
 
-                    <label>Endereço: OBG
-                        <Input type="text" name="endereco" value={dadosFornecedor.endereco || ""}  onChange={handleChange} placeholder="Endereço" required /><br />
-                    </label>
+                                <Form.Group controlId="senha" className="mb-3">
+                                    <Form.Label className="text-light">Senha: OBG</Form.Label>
+                                    <Form.Control type="password" name="senha" value={dadosFornecedor.senha || ""} onChange={handleChange} placeholder="Senha" required />
+                                    {errors.senha && <Alert variant="danger">{errors.senha}</Alert>}
+                                </Form.Group>
 
-                    <label>Telefone: OBG
-                        <Input type="text" name="telefone" value={dadosFornecedor.telefone || ""}  onChange={handleChange} placeholder="Telefone" required /><br />
-                    </label>
+                                <Form.Group controlId="confirma_senha" className="mb-3">
+                                    <Form.Label className="text-light">Confirma Senha: OBG</Form.Label>
+                                    <Form.Control type="password" name="confirma_senha" value={dadosFornecedor.confirma_senha || ""} onChange={handleChange} placeholder="Confirma Senha" required />
+                                    {errors.confirma_senha && <Alert variant="danger">{errors.confirma_senha}</Alert>}
+                                </Form.Group>
 
-                    <label>E-mail: OBG
-                        <Input type="email" name="email" value={dadosFornecedor.email || ""}  onChange={handleChange} placeholder="E-mail" required /><br />
-                    </label>
-
-                    <label>Senha: OBG
-                        <Input type="password" name="senha" value={dadosFornecedor.senha || ""}  onChange={handleChange} placeholder="Senha" required /><br />
-                    </label>
-
-                    <label>Confirma Senha: OBG
-                        <Input type="password" name="confirma_senha" value={dadosFornecedor.confirma_senha || ""}  onChange={handleChange} placeholder="Senha" required /><br />
-                    </label>
-
-                    <input type="submit" />
-                    <Button>Cadastrar</Button>
-                    <BotaoPrincipal>MEU NOME</BotaoPrincipal>
-                </form>
-            </Wrapper>
-        </div>
-    )
+                                <Button type="submit" className="w-100 mt-3" style={{ backgroundColor: 'yellow', borderColor: 'yellow', color: 'black' }}>Cadastrar</Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
-export default CadastroFornecedor
+export default CadastroFornecedor;
+
